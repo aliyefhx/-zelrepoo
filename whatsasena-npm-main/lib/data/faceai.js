876834f578fb8233edd004501e970da109534f80
safiye5151 <90639434+safiye5151@users.noreply.github.com>
2021-12-08 19:03:25 +0300

    Delete whatsasena-npm-main directory

--- whatsasena-npm-main/lib/data/faceai.js
+++
@@ -1,8 +0,0 @@
-const exec = require('child_process').exec;
-const os = require("os");
-async function face_generator() {
-
-  var data = 'https://thispersondoesnotexist.com/image?' + (new Date).getTime()
-  return data;
-}
-module.exports = face_generator
